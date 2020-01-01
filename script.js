457341820a18ea82e0e2d4b603f711539a8d6e1f
Jeremy Savage <52190825+jeremyssavage@users.noreply.github.com>
2020-01-01 18:20:08 +1000

    Update script.js
    Copying over some draft code I'll need later so as not to lose it.

--- script.js
+++ script.js
@@ -23,3 +23,35 @@ request.onload = function() {
 }
 
 request.send()
+
+
+
+
+    let xml = request.responseXML
+    
+    let sMinPlayers = xml.getElementsByTagName('minplayers')[0].getAttribute('value')
+    let sMaxPlayers = xml.getElementsByTagName('maxplayers')[0].getAttribute('value')
+    let nMinPlayers = Number(sMinPlayers)
+    let nMaxPlayers = Number(sMaxPlayers)
+    
+    let b=0, r=0, nr=0
+  	let resultsArray = xml.getElementsByTagName('results')
+    for (let resultsNode of resultsArray) {
+      sNumPlayers = resultsNode.getAttribute('numplayers')
+      nNumPlayers = Number(sNumPlayers)
+      if (nMinPlayers <= nNumPlayers && nNumPlayers <= nMaxPlayers) {
+        let resultArray = resultsNode.getElementsByTagName('result')
+        for (let resultNode of resultArray) {
+          let value = resultNode.getAttribute('value')
+          let numvotes = resultNode.getAttribute('numvotes')
+          if (value === 'Best') {
+            b = numvotes
+          } else if (value === 'Recommended') {
+            r = numvotes
+          } else if (value === 'Not Recommended') {
+            nr = numvotes
+          }
+        }
+        document.write(sNumPlayers + ' B: ' + b + ' R: ' + r + ' NR: ' + nr + '<br>')
+	    }
+	  }
