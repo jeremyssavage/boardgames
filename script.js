a16d97df9885026f664db183139e9f1ceec4634b
Jeremy Savage <52190825+jeremyssavage@users.noreply.github.com>
2020-01-01 17:55:04 +1000

    Create script.js
    Testing XML pull request and (hopefully) spitting out number of board games to console.
    
    No means for choosing the username (Savdog is hard-coded) or retrying the request. Will have to include a form and button for that...

---
+++ script.js
@@ -0,0 +1,27 @@
+<script>
+
+let request = new XMLHttpRequest()  // Object used to pull data from BGG's API.
+let urlBase = 'https://api.geekdo.com/xmlapi2/'  // The base for all BGG XML data. JS has fancy URL objects & methods but I don't think I need them...
+
+let username = 'Savdog'  // Should be able to change this eventually...
+request.open('GET', urlBase + 'collection?username='+username+'&excludesubtype=boardgameexpansion')  // Prepare connection to user's collection.
+request.responseType = 'document'  // Specifies XML output, but not sure this is entirely necessary.
+
+// Event handling for the following request.send() command.
+request.onerror = () => console.log('Error fetching data for ' + username + '. Please refresh your browser to retry.')
+request.onload = function() {
+  if (request.status != 200) {
+    console.log('XMLHttpRequest error: ' + request.status)
+  } else {
+    console.log('XMLHttpRequest successful!')
+    // MAIN STUFF HAPPENS HERE =---------------------------------------------------------------------------------------------------------------
+    let xmlColl = request.responseXML  // Contains the collection data in XML format.
+  
+    totalitems = xmlColl.documentElement.getAttribute('totalitems')
+    console.log('Found ' + totalitems + ' board games (excluding expansions).')
+  }
+}
+
+request.send()
+
+</script>
